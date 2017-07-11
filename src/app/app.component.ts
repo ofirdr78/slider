2fcb9724262f7b76e023398471f48e7c028aa23d
Ofir Dror <odror@interwise.com>
2017-07-11 12:59:35 +0300

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  volume: number;
+
+  constructor() {
+    this.volume = 50;
+  }
+
+
+  onSubmit(x) {
+    console.log(x);
+    this.volume = x;
+  }
 }
